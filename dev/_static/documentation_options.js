af6a991ebab4d295b2dd4f204d058f6b68b28982
deeplycloudy <deeplycloudy@users.noreply.github.com>
2021-04-08 21:17:15 +0000

    Deploy dev to GitHub Pages

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.0+215.g491f592',
+    VERSION: '0.7.0+216.ga9410ad',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
